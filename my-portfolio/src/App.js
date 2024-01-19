c4bb0f75ac5c649943d4c84da8e15639e5fd0d3a
Alenis Chavarria <112582758+alenischavarria@users.noreply.github.com>
2024-01-18 20:13:33 -0600

    name and job aspirations with animations

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -23,18 +23,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <NavBar />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="myName">ALENIS CHAVARRIA</h1>
+        <div className="sliding-box">
+        <h1 className="myJobAspirations">SOFTWARE ENGINEER, <br/> FULL - STACK, APP, WEB, & UX/UI DEVELOPER</h1>
+        </div>
+      </header> 
+      <body>
+        <p>COMING SOON</p>
+      </body>
     </div>
   );
 }
